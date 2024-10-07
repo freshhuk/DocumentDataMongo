e802983aa77b178256137b75b82f32e55b965d60
freshhuk <freshhuk13@gmail.com>
2024-10-08 00:45:44 +0300

    Initial commit

---
+++ src/main/java/com/document/documentdatamongo/DocumentDataMongoApplication.java
@@ -0,0 +1,13 @@
+package com.document.documentdatamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentDataMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentDataMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/document/documentdatamongo/DocumentDataMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.document.documentdatamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentDataMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
